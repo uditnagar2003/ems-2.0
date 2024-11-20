50112af7f1f1f3a9f4823875fe233212b76400b8
uditnagar2003 <uditnagar2003@gmail.com>
2024-11-20 23:13:06 +0530

    20 nov general department added

--- client/Program.cs
+++ client/Program.cs
@@ -6,6 +6,10 @@ using ClientLibrary.Helpers;
 using Microsoft.AspNetCore.Components.Authorization;
 using ClientLibrary.Services.contract;
 using ClientLibrary.Services.Implementations;
+using Syncfusion.Blazor.Popups;
+using Syncfusion.Blazor;
+using client.ApplicationStates;
+//using Syncfusion.Blazor.Popups;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -23,5 +27,12 @@ builder.Services.AddScoped<LocalStorageService>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<IuserAccountService, UserAccountService>();
 //
+
+builder.Services.AddScoped<DepartmentState>();
+
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddScoped<SfDialogService>();
+ 
+//builder.Services.AddScoped<SfDialogService>();
 //builder.Services.AddScoped<Serializations>();
 await builder.Build().RunAsync();
